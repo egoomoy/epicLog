0f5b238d20b7be890529d01804df41c095354bb5
egoomoy <egoomoy@gmail.com>
2020-03-25 19:55:33 +0900

    reboot

--- src/graphql/User.ts
+++ src/graphql/User.ts
@@ -1,30 +1,16 @@
 import { gql, IResolvers } from 'apollo-server-koa';
 import { getRepository } from 'typeorm';
 import { ApolloContext } from '../app';
-<<<<<<< HEAD
 import { User } from '../entity/User';
 import { setTokenCookie } from '../lib/token';
-=======
-import { Profile } from '../entities/Profile';
-import { User } from '../entities/User';
->>>>>>> cc8c9a8a42f59209febe00759c4220a42b417b86
 
 export const typeDefs = gql`
   extend type Query {
     me: String
   }
   extend type Mutation {
-<<<<<<< HEAD
     signUp(email: String!, password: String!): mutationResponse!
     signIn(email: String!, password: String!): mutationResponse!
-=======
-    emailSignUpUser(
-      email: String!
-      password: String!
-      name: String!
-      shortBio: String
-    ): emailSignUpUserResponse!
->>>>>>> cc8c9a8a42f59209febe00759c4220a42b417b86
   }
   type mutationResponse {
     ok: Boolean!
@@ -35,35 +21,17 @@ export const typeDefs = gql`
 export const resolvers: IResolvers<any, ApolloContext> = {
   Query: {
     me: async (_parent: any, _args: any, _context: ApolloContext, _info: any) => {
-<<<<<<< HEAD
       try {
         return 'test user';
-=======
-      const repo = getRepository(User);
-      try {
-        const user = await repo.findOne({
-          where: {
-            id: _context.user_id
-          }
-        });
-        if (user) return user.id;
->>>>>>> cc8c9a8a42f59209febe00759c4220a42b417b86
       } catch (e) {
         console.log(e);
       }
     }
   },
   Mutation: {
-<<<<<<< HEAD
     signUp: async (_parent: any, args: any, _context: any, _info: any) => {
       const { email, password } = args;
-=======
-    emailSignUpUser: async (_parent: any, args: any, _context: any, _info: any) => {
-      const { email, password, name, shortBio } = args;
-
->>>>>>> cc8c9a8a42f59209febe00759c4220a42b417b86
       const userRepo = getRepository(User);
-      const profileRepo = getRepository(Profile);
       try {
         const existUser = await userRepo.findOne({ email });
         if (existUser) {
@@ -71,28 +39,9 @@ export const resolvers: IResolvers<any, ApolloContext> = {
             ok: false,
             error: '로그인하세요.'
           };
-<<<<<<< HEAD
         }
         try {
           await userRepo.create({ email, password }).save();
-=======
-        } else {
-          try {
-            const user = await userRepo.create({ email, password }).save();
-            await profileRepo
-              .create({
-                shortBio,
-                name,
-                fk_user_id: user.id
-              })
-              .save();
-          } catch (error) {
-            return {
-              ok: false,
-              error: error.message
-            };
-          }
->>>>>>> cc8c9a8a42f59209febe00759c4220a42b417b86
           return {
             ok: true,
             error: null
